bb7698eee609bc886dd6e7d8a9a37a438250ff1e
abdushukurova <abdushukurova.km@gmail.com>
2021-06-13 19:16:59 +0300

    fix game build

--- src/core/game.cpp
+++ src/core/game.cpp
@@ -1,16 +1,31 @@
 #include "core/engine.h"
 #include "core/input_system.h"
 #include "core/world.h"
+#include "core/components/mesh_component.h"
+#include "core/components/camera_component.h"
 #include "core/plugins_registry.h"
 #include "core/renderer.h"
 #include "gfx/gfx.h"
 #include "platform/window.h"
 #include "base/window_event.h"
 #include "core/assets/assets.h"
+#include "core/meta/registration.h"
+#include "core/meta/schema.h"
 
 extern void load_plugins(engine*);
 
 int main(int argv, char* argc[]) {
+
+  char schema_dir[256];
+  std::strcpy(schema_dir, std::filesystem::current_path().c_str());
+  std::strcat(schema_dir, "/schema");
+
+  meta::load_schemas(schema_dir);
+
+  register_type(link_component);
+  register_type(transform_component);
+  register_type(camera_component);
+  register_type(mesh_component);
 
   assets::init(std::filesystem::current_path().c_str());
 
