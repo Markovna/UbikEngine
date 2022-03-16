85838b35665640bae2c82c39502acefa79bca9aa
abdushukurova <abdushukurova.km@gmail.com>
2022-03-17 02:35:47 +0300

    update experimental.cpp

--- src/gfx/experimental/experimental.cpp
+++ src/gfx/experimental/experimental.cpp
@@ -25,6 +25,8 @@
 #include <core/meta/interface_registry.h>
 #include <core/schema.h>
 #include <base/color.h>
+#include <core/dcc_asset.h>
+#include <editor/editor_tab_manager.h>
 #include "core/asset_repository.h"
 
 int main(int argc, char* argv[]) {
@@ -45,49 +47,79 @@ int main(int argc, char* argv[]) {
   auto input_system = registry.set<::input_system>(std::make_unique<::input_system>());
   auto viewer_registry = registry.set<::viewer_registry>(std::make_unique<::viewer_registry>());
   auto schema_registry = registry.set<::schema_registry>(std::make_unique<::schema_registry>());
+  auto gui_renderer = registry.set<::gui>(std::make_unique<::gui>(registry));
   auto editor_tab_manager = registry.set<::editor_tab_manager>(std::make_unique<::editor_tab_manager>(registry));
 
-  load_assets(*assets_filesystem, *assets_repository, { ".entity", ".meta", ".shader", ".schema" });
+  load_assets(*assets_filesystem, *assets_repository, { ".entity", ".shader", ".schema", ".texture", ".dcc_asset" });
+
+  // TODO
+  create_and_compile_texture_asset("assets/textures/container.jpg", "assets/textures/container.texture", *assets_repository, *assets_filesystem);
+  create_and_compile_texture_asset("assets/textures/seal.png", "assets/textures/seal.texture", *assets_repository, *assets_filesystem);
 
   compile_shaders(*assets_repository, *assets_filesystem);
-  compile_textures({".jpg", ".png"}, *assets_repository, *assets_filesystem);
+
+  constexpr const char* backpack_asset_path = "assets/scenes/backpack.dcc_asset";
+  constexpr const char* backpack_entity_path = "assets/scenes/backpack.entity";
+  if (asset_id id = create_dcc_asset(fs::to_project_path("assets/scenes/backpack/backpack.obj"), *assets_repository)) {
+    if (auto* asset = assets_repository->get_asset_by_path(backpack_asset_path)) {
+      assets_repository->destroy_asset(asset->id());
+    }
+
+    assets_repository->set_asset_path(id, backpack_asset_path);
+    assets_filesystem->save(*assets_repository, backpack_asset_path, true);
+  }
+
+  if (asset_id id = create_entity_from_dcc_asset(*assets_repository->get_asset_by_path(backpack_asset_path), *assets_repository, *assets_filesystem)) {
+    if (auto* asset = assets_repository->get_asset_by_path(backpack_entity_path)) {
+      assets_repository->destroy_asset(asset->id());
+    }
+
+    assets_repository->set_asset_path(id, backpack_entity_path);
+    assets_filesystem->save(*assets_repository, backpack_entity_path, true);
+  }
 
   schema_builder(meta::get_typeid<vec3>())
       .add("x", schema_type::FLOAT)
       .add("y", schema_type::FLOAT)
       .add("z", schema_type::FLOAT)
-      .build(*schema_registry);
+    .build(*schema_registry);
+
+  schema_builder(meta::get_typeid<quat>())
+      .add("x", schema_type::FLOAT)
+      .add("y", schema_type::FLOAT)
+      .add("z", schema_type::FLOAT)
+      .add("w", schema_type::FLOAT)
+    .build(*schema_registry);
 
   schema_builder(meta::get_typeid<transform>())
       .add("position", schema_type::OBJECT, meta::get_typeid<vec3>())
       .add("rotation", schema_type::OBJECT, meta::get_typeid<quat>())
       .add("scale", schema_type::OBJECT, meta::get_typeid<vec3>())
-      .build(*schema_registry);
+    .build(*schema_registry);
 
   schema_builder(meta::get_typeid<color>())
       .add("r", schema_type::FLOAT)
       .add("g", schema_type::FLOAT)
       .add("b", schema_type::FLOAT)
       .add("a", schema_type::FLOAT)
-      .build(*schema_registry);
+    .build(*schema_registry);
 
-  build_schema_from_asset(meta::get_typeid<transform_component>(), *assets_repository->get_asset("schemas/transform_component.schema"), *schema_registry);
-  build_schema_from_asset(meta::get_typeid<mesh_component>(), *assets_repository->get_asset("schemas/mesh_component.schema"), *schema_registry);
+  build_schema_from_asset(meta::get_typeid<transform_component>(), *assets_repository->get_asset_by_path("schemas/transform_component.schema"), *schema_registry);
+  build_schema_from_asset(meta::get_typeid<mesh_component>(), *assets_repository->get_asset_by_path("schemas/mesh_component.schema"), *schema_registry);
 
   window window({512, 512});
 
   swap_chain swap_chain = renderer->create_swap_chain(window.get_handle());
 
   auto resource_command_buffer = renderer->create_resource_command_buffer();
-  shader_repository->compile(*assets_repository->get_asset("assets/shaders/TestShader.shader"), assets_repository.get(), resource_command_buffer.get());
-  shader_repository->compile(*assets_repository->get_asset("assets/shaders/GUIShader.shader"), assets_repository.get(), resource_command_buffer.get());
-  shader_repository->compile(*assets_repository->get_asset("assets/shaders/EditorGrid.shader"), assets_repository.get(), resource_command_buffer.get());
+  shader_repository->compile(*assets_repository->get_asset_by_path("assets/shaders/TestShader.shader"), assets_repository.get(), resource_command_buffer.get());
+  shader_repository->compile(*assets_repository->get_asset_by_path("assets/shaders/GUIShader.shader"), assets_repository.get(), resource_command_buffer.get());
+  shader_repository->compile(*assets_repository->get_asset_by_path("assets/shaders/EditorGrid.shader"), assets_repository.get(), resource_command_buffer.get());
   renderer->submit(*resource_command_buffer);
 
   init_world(registry);
   init_render_pipeline(registry);
   register_components(registry);
-  load_gui(registry);
 
   fs::path libs_folder = fs::to_project_path(".ubik/libs");
   library_loader libs;
@@ -99,11 +131,10 @@ int main(int argc, char* argv[]) {
   auto app = registry.get<application>();
   app->start();
 
+  gui_renderer->init(&window);
+  connect_gui_events(*gui_renderer, *input_system);
+
   world world;
-
-  gui gui_renderer { &window };
-
-  connect_gui_events(gui_renderer, *input_system);
 
   auto simulation_view = registry.view<simulation>();
 
@@ -122,10 +153,10 @@ int main(int argc, char* argv[]) {
 
     window_event event;
     while (window.poll_event(event)) {
-      if (event.type == event_type::Close) {
+      if (event.type() == event_type::Close) {
         running = false;
-      } else if (event.type == event_type::Resize) {
-        renderer->resize_swap_chain(swap_chain, event.resize.resolution);
+      } else if (event.type() == event_type::Resize) {
+        renderer->resize_swap_chain(swap_chain, static_cast<const resize_event&>(event).resolution);
       }
       input_system->push_event(event);
     }
@@ -138,19 +169,18 @@ int main(int argc, char* argv[]) {
     }
 
     viewer_registry->clear();
+
     renderer->begin_frame();
 
-    gui_renderer.begin();
-
-    gui_begin_dockspace();
+    gui_renderer->begin();
 
     editor_tab_manager->update();
 
     render_pipeline->render(viewer_registry->begin(), viewer_registry->end(), *renderer);
 
-    window.set_cursor(gui_renderer.cursor());
+    window.set_cursor(gui_renderer->cursor());
 
-    gui_renderer.end(swap_chain.back_buffer_handle);
+    gui_renderer->end(swap_chain.back_buffer_handle);
 
     renderer->swap(swap_chain);
   }
@@ -159,7 +189,7 @@ int main(int argc, char* argv[]) {
     sim->stop();
   }
 
-  disconnect_gui_events(gui_renderer, *input_system);
+  disconnect_gui_events(*gui_renderer, *input_system);
 
   app->stop();
 
